11cf64aa22e93689e507e8151ecb5ddc17ed7778
haxpak <haxpak@gmail.com>
2019-11-25 08:26:37 +0530

    redirection after submitting query

--- website/routes/webroutes.js
+++ website/routes/webroutes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
   app.route('/').get(controller.index);
   app.route('/index').get(controller.index);
 
-  app.route('/thankyou').get(controller.thanks);
+  app.route('/thankyou').get(controller.thankyou);
 
   // route for get enquiry page
   app.route('/enquire').get(controller.enquiry);
