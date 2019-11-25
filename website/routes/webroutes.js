e43b22f4362b008b8dfc40a26f8480e207d950e3
haxpak <haxpak@gmail.com>
2019-11-25 06:55:30 +0530

    added /index route

--- website/routes/webroutes.js
+++ website/routes/webroutes.js
@@ -5,6 +5,7 @@ module.exports = function(app) {
     var controller = require('../controllers/mainController');
      //Website Routes
   app.route('/').get(controller.index);
+  app.route('/index').get(controller.index);
 
   // route for get enquiry page
   app.route('/enquire').get(controller.enquiry);
